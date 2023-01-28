80c37f7f6b7ef6f2c69f5678f8e275b446f9ee1f
Rufus100Procent <Maxamadabdulahist@outlook.com>
2023-01-28 13:34:59 +0100

    "comit"

--- backend/src/main/java/se/jensenyh/javacourse/saltmerch/backend/Main.java
+++ backend/src/main/java/se/jensenyh/javacourse/saltmerch/backend/Main.java
@@ -7,10 +7,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @SpringBootApplication
 public class Main extends SpringBootServletInitializer {
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(Main.class);
-    }
+
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
